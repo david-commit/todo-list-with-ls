a81c8aa124c9df83e4fa7f9d44eec9eeabc50025
david-commit <davidondiege@gmail.com>
2023-03-02 12:16:10 +0300

    ToDo App full UI, half functionality

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { CATEGORIES, TASKS } from './data';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCat, setSelecedCat] = useState('All');
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    // Store tasks
+    localStorage.setItem('tasks', JSON.stringify(TASKS));
+    setTasks(JSON.parse(localStorage.getItem('tasks')));
+    // Store categories
+    localStorage.setItem('categories', JSON.stringify(CATEGORIES));
+    setCategories(JSON.parse(localStorage.getItem('categories')));
+  }, []);
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='App'>
+      <div className='todo-container'>
+        <form onSubmit={handleAddTask}>
+          <h1>To-Do List</h1>
+          <p>Manage your Activities</p>
+          <br />
+          <div>
+            <select onChange={(e) => setSelecedCat(e.target.value)}>
+              <option hidden>Category</option>
+              {categories.map((cat) => {
+                return (
+                  <option value={cat} key={cat}>
+                    {cat}
+                  </option>
+                );
+              })}
+            </select>
+            <input type='text' placeholder='Add Task' autoFocus />
+            <button type='submit'>+</button>
+          </div>
+        </form>
+        <hr />
+        <div className='task-list'>
+          {categories.map((cat) => {
+            return (
+              <button value={cat} key={cat}>
+                {cat}
+              </button>
+            );
+          })}
+          <br />
+          <div class='task-cards'>
+            {tasks.length > 0 ? (
+              tasks.map((task) => {
+                return (
+                  <div class='task-card'>
+                    <span>
+                      <input type='checkbox' value={checked} onChange={() => setChecked(!checked)} />
+                      <label class={checked ? 'checked' : 'unchecked'}>
+                        {task.text}
+                      </label>
+                    </span>
+                    <i class='fa-solid fa-trash'></i>
+                  </div>
+                );
+              })
+            ) : (
+              <h3 class='hooray'>Hooray! You dont have any pending tasks!</h3>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const CATEGORIES = ['All', 'Code', 'Food', 'Money', 'Misc'];
+
+export const TASKS = [
+  {
+    id: 1,
+    text: 'Buy rice',
+    category: 'Food',
+    checked: false,
+  },
+  {
+    id: 2,
+    text: 'Save a tenner',
+    category: 'Money',
+    checked: false,
+  },
+  {
+    id: 3,
+    text: 'Build a todo app',
+    category: 'Code',
+    checked: false,
+  },
+  {
+    id: 4,
+    text: 'Build todo API',
+    category: 'Code',
+    checked: false,
+  },
+  {
+    id: 5,
+    text: 'Get an ISA',
+    category: 'Money',
+    checked: false,
+  },
+  {
+    id: 6,
+    text: 'Cook rice',
+    category: 'Food',
+    checked: false,
+  },
+  {
+    id: 7,
+    text: 'Tidy house',
+    category: 'Misc',
+    checked: false,
+  },
+  
+];
